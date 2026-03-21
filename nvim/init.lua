fc9147c75c19d7585c3426579eb51e319ddfea9b
Divyansh Kataria <217927899+divyansh-kataria@users.noreply.github.com>
2026-03-21 11:52:27 +0530

    added multiple new files

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+vim.opt.termguicolors = false
+
+
+vim.cmd("highlight Normal guibg=NONE")
+
+vim.cmd("highlight NormalNC guibg=NONE")
+
+vim.cmd("highlight EndOfBuffer guibg=NONE")
+
+vim.cmd("highlight SignColumn guibg=NONE")
+
+
