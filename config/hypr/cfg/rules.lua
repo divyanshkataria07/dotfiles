401d2c950faae7226a88ed44849e6b540e5f3d99
Divyansh Kataria <217927899+divyansh-kataria@users.noreply.github.com>
2026-05-03 23:14:08 +0530

    added opacity rules and fixed regex

--- config/hypr/cfg/rules.lua
+++ config/hypr/cfg/rules.lua
@@ -2,7 +2,7 @@
 --  WINDOW & WORKSPACE RULES
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
--- 1. SYSTEM ESSENTIALS
+-- System Essentials
 hl.window_rule({
 	name = "suppress-maximize",
 	match = { class = ".*" },
@@ -29,35 +29,35 @@ hl.window_rule({
 	float = true,
 })
 
--- 2. THE RICE (Opacity & Blur)
+-- Opacity Rule
 hl.window_rule({
-	name = "rice-opacity",
 	match = {
-		class = [[^org\.gnome\..*|org\.cachyos\..*|localsend|thunderbird|obsidian|org\.qbittorrent\.qBittorrent|btrfs-assistant|io\.ente\.auth|org\.pwmt\.zathura|code|steam|Spotify|discord]],
+		class = [[^(org\.gnome\..*|org\.cachyos\..*|localsend|thunderbird|obsidian|org\.qbittorrent\.qBittorrent|btrfs-assistant|io\.ente\.auth|org\.pwmt\.zathura|code|steam|Spotify|discord)$]],
 	},
-	opacity = "0.88 0.88",
+	opacity = 0.88,
 })
 
--- 3. FLOATING & MEDIA
+-- Floating Rule
 hl.window_rule({
 	name = "floating-utils",
-	match = { class = "org.pulseaudio.pavucontrol|com.rafaelmardojai.Blanket|xdg-desktop-portal-gtk" },
+	match = { class = [[^(org\.pulseaudio\.pavucontrol|com\.rafaelmardojai\.Blanket|xdg-desktop-portal-gtk)$]] },
 	float = true,
 	center = true,
+	opacity = 0.88,
 })
 
+-- PiP floating rule
 hl.window_rule({
 	name = "Picture-in-Picture",
 	match = { title = "^([Pp]icture[-s]?[Ii]n[-s]?[Pp]icture)(.*)$" },
 	float = true,
 	pin = true,
-	opacity = "1.0 1.0",
+	opacity = 1.0,
 })
 
--- 4. LAYER RULES
+-- Layer Rules
 --hl.layer_rule({
 --	name = "noctalia-shell-blur",
 --	match = { namespace = "^noctalia-.*$" },
 --	blur = true,
---	ignorezero = true,
 --})
